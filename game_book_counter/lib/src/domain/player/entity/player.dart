e299d9973d3211994a6502738561e72f13f968ef
Edianio Ferreira <edianioferreira2@gmail.com>
2023-01-14 09:35:31 -0300

    start

---
+++ game_book_counter/lib/src/domain/player/entity/player.dart
@@ -0,0 +1,8 @@
+class Player {
+  final String id;
+  final String name;
+  final String? alias;
+  final String email;
+
+  Player({required this.id, required this.name, this.alias, required this.email});
+}
